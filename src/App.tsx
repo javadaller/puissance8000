a2aa60a49296503ce4f2bf1a3a1403254289053d
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-09-13 16:19:52 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client'
 
 import './sass/style.scss'
 
-import  Header  from './components/header/header'
-import  Home  from './components/home/home'
-import  Footer  from './components/footer/footer'
+import { init } from './ts/init'
 
+import  Header  from './components/header/header'
+import  Main  from './components/main/main'
+import  Footer  from './components/footer/footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
-    <Home />
+    <Main />
     <Footer />
   </React.StrictMode>,
 )
+
+init()
---
+++ src/components/main/main.tsx
@@ -0,0 +1,13 @@
+import './main.scss'
+
+function Main() {
+  return (
+    <>
+    <main>
+      <p>Home</p>
+    </main>  
+    </>
+  )
+}
+
+export default Main
